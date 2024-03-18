6d230d2752277b01799275abc2ace32607931d53
rishikeshvijay1999 <144434263+rishikeshvijay1999@users.noreply.github.com>
2024-03-18 12:16:47 +0530

    Update Calculator.java

--- src/main/java/mypackage/Calculator.java
+++ src/main/java/mypackage/Calculator.java
@@ -25,7 +25,7 @@ public class Calculator extends HttpServlet {
 
     private Connection getDBConnection() throws SQLException {
         // Update with your database connection details
-        String jdbcUrl = "jdbc:mysql://192.168.75.158:3306/mydb";
+        String jdbcUrl = "jdbc:mysql://192.168.75.160:3306/mydb";
         String jdbcUser = "mysql";
         String jdbcPassword = "mysql@123";
 
